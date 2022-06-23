08540c9a9d816d2728f0d1d9e2489e5ae5d7b75b
ConchDev <unsoughtconch@gmail.com>
2022-06-22 22:39:01 -0400

    Add argparse for easy development testing

--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 import os
 import discord
 from discord.ext import commands
+import argparse
+
+# Add an argument parser to handle production and development mode
+parser = argparse.ArgumentParser(description="Discord bot for the Discord server")
+parser.add_argument("--dev", action="store_true", help="Run the bot in development mode")
+args = parser.parse_args()
+
+if args.dev:
+    import dotenv
+    dotenv.load_dotenv()
+    
 
 bot = commands.Bot(command_prefix='cb ', intents=discord.Intents.all())
 
@@ -11,4 +22,4 @@ bot = commands.Bot(command_prefix='cb ', intents=discord.Intents.all())
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-bot.run(os.environ["DISCORD_TOKEN"])
+bot.run(os.environ.get("DISCORD_TOKEN"))
