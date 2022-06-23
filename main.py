ba91d62219b2a3e94def7f4376376b6f95fb410d
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-06-23 04:30:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='cb ', intents=discord.Intents.all())
 
+async def load_cogs(bot):
+    for file in os.listdir('.cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(file)
 
 @bot.event
 async def on_ready():
+    await load_cogs(bot)
     print(f"Logged in as {bot.user}")
 
 bot.run(os.environ["DISCORD_TOKEN"])
